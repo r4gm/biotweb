156307670ef20712a9fb89e6c79ef2079c0d8d4c
KuroInkH <llopezberist@gmail.com>
2021-03-24 23:12:28 -0600

    Responsive serviciosv2

--- assets/script/slider.js
+++ assets/script/slider.js
@@ -1,45 +1,68 @@
-var counter = 2;
-var controls = document.getElementById('controls');
-var rad = document.getElementById('bullets');
+$(function() {
+  var $slider = $('.slides');
 
-setInterval(function() {
-  document.getElementById('slide' + counter).checked = true;
-  counter++;
-  if (counter > 4) {
-    counter = 1;
+  showSlides();
+
+  function showSlides() {
+    var interval = setInterval(function() {
+      goToSlide()
+    }, 6500);
   }
-}, 6500);
 
-controls.onclick = function() {
-  setTimeout(() => {
-    counter = $("input[name='slider']:checked").val();
-    console.log("counter " + counter);
-  }, 2000);
+  function goToSlide() {
+    $slider.stop(true).animate({left: '-100%'}, 800, "swing", function() {
+      $slider.css({left: '0'});
+      $slider.append($slider.find('> div').eq(0));
+    });
+  }
+});
 
-}
 
-rad.onclick = function() {
-  setTimeout(() => {
-    counter = $("input[name='slider']:checked").val();
-    console.log("counter " + counter);
-  }, 2000);
-}
 
-// $(function() {
-//   var $slider = $('.slides');
+
+
+// var counter = 2;
+// var controls = document.getElementById('controls');
+// var rad = document.getElementById('bullets');
 //
-//   showSlides();
-//
-//   function showSlides() {
-//     var interval = setInterval(function() {
-//       goToSlide()
-//     }, 4000);
+// setInterval(function() {
+//   document.getElementById('slide' + counter).checked = true;
+//   counter++;
+//   if (counter > 4) {
+//     counter = 1;
 //   }
+// }, 6500);
 //
-//   function goToSlide() {
-//     $slider.stop(true).animate({left: '-100%'}, 800, "swing", function() {
-//       $slider.css({left: '0'});
-//       $slider.append($slider.find('> div').eq(0));
-//     });
-//   }
-// });
+// controls.onclick = function() {
+//   setTimeout(() => {
+//     counter = $("input[name='slider']:checked").val();
+//     console.log("counter " + counter);
+//   }, 2000);
+//
+// }
+//
+// rad.onclick = function() {
+//   setTimeout(() => {
+//     counter = $("input[name='slider']:checked").val();
+//     console.log("counter " + counter);
+//   }, 2000);
+// }
+//
+// // $(function() {
+// //   var $slider = $('.slides');
+// //
+// //   showSlides();
+// //
+// //   function showSlides() {
+// //     var interval = setInterval(function() {
+// //       goToSlide()
+// //     }, 4000);
+// //   }
+// //
+// //   function goToSlide() {
+// //     $slider.stop(true).animate({left: '-100%'}, 800, "swing", function() {
+// //       $slider.css({left: '0'});
+// //       $slider.append($slider.find('> div').eq(0));
+// //     });
+// //   }
+// // });

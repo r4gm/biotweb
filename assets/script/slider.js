0d272691eade592eb126d3842c6c51b1ac1a2286
KuroInkH <llopezberist@gmail.com>
2021-03-25 00:07:53 -0600

    Banner

--- assets/script/slider.js
+++ assets/script/slider.js
@@ -1,21 +1,40 @@
-$(function() {
-  var $slider = $('.slides');
-
-  showSlides();
-
-  function showSlides() {
-    var interval = setInterval(function() {
-      goToSlide()
-    }, 6500);
+var counter = 2;
+setInterval(function(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if(counter > 4){
+    counter = 1;
   }
+}, 6000);
 
-  function goToSlide() {
-    $slider.stop(true).animate({left: '-100%'}, 800, "swing", function() {
-      $slider.css({left: '0'});
-      $slider.append($slider.find('> div').eq(0));
-    });
-  }
-});
+
+
+
+
+
+
+
+
+
+
+// $(function() {
+//   var $slider = $('.slides');
+//
+//   showSlides();
+//
+//   function showSlides() {
+//     var interval = setInterval(function() {
+//       goToSlide()
+//     }, 6500);
+//   }
+//
+//   function goToSlide() {
+//     $slider.stop(true).animate({left: '-100%'}, 800, "swing", function() {
+//       $slider.css({left: '0'});
+//       $slider.append($slider.find('> div').eq(0));
+//     });
+//   }
+// });
 
 
 
